390fd3fd2f8640bbea3d085401092eb8305f7fee
viqi zevic <vicky.tanzil@gmail.com>
2018-09-02 19:40:08 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@
 
 function forLoop(array) {
   for (var i = 1; i < 25; i++) {
-    array.push(`"I am ${i} strange loop${i === 1 ? '' : 's'}."`)
+    array.push(`I am ${i} strange loop${i === 1 ? '' : 's'}.`)
   }
   return array
 }
